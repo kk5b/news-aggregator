5625b78598a2158b388313955531ca1883b6627d
kk5b <karthikeyanbofficial5@gmail.com>
2025-07-03 12:24:49 +0530

    vercel.json updated for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path" // <-- Import path
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
   resolve: {
     alias: {
