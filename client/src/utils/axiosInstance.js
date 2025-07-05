1ce6621a4ac84cd348061d6dd18f2c7ab0ccfe45
kk5b <karthikeyanbofficial5@gmail.com>
2025-07-05 09:44:21 +0530

    vercel deployment changes done

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: '/api',
+    baseURL: import.meta.env.VITE_API_BASE_URL + '/api',
 });
 
 // This interceptor correctly adds the auth token to every request
--- server/index.js
+++ server/index.js
@@ -9,6 +9,24 @@ app.use(cors());
 app.use(express.json());
 app.use(globalLimiter);
 
+// CORS Configuration
+const allowedOrigins = [
+    'https://news-aggregator-two-gray.vercel.app/',
+    'http://localhost:5173'
+];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+};
+
+app.use(cors(corsOptions));
+
 // Routes must NOT have the /api prefix here
 app.use('/auth', authLimiter, require('./routes/auth'));
 app.use('/news', require('./routes/news'));
