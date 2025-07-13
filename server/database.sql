0676b6c6557ffdc85a555fefa5b43641197b89a8
kk5b <karthikeyanbofficial5@gmail.com>
2025-07-13 21:33:50 +0530

    database update

--- server/database.sql
+++ server/database.sql
@@ -1,5 +1,4 @@
-CREATE DATABASE news_aggregator;
-
+-- Create the users table
 CREATE TABLE users (
     user_id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
     username VARCHAR(255) NOT NULL,
@@ -10,9 +9,11 @@ CREATE TABLE users (
         TIME ZONE DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP
     WITH
-        TIME ZONE DEFAULT CURRENT_TIMESTAMP
+        TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+        preferences JSONB DEFAULT '{"categories": [], "sources": []}'
 );
 
+-- Create the bookmarks table
 CREATE TABLE bookmarks (
     bookmark_id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
     user_id UUID NOT NULL,
@@ -23,6 +24,7 @@ CREATE TABLE bookmarks (
         FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
 
+-- Create the view_history table
 CREATE TABLE view_history (
     history_id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
     user_id UUID NOT NULL,
@@ -35,4 +37,5 @@ CREATE TABLE view_history (
         FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
 
+-- Create an index on the view_history table for faster lookups
 CREATE INDEX idx_view_history_user_id ON view_history (user_id);
